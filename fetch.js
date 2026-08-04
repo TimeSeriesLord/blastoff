c5c9191413ba3a3944029715cbda1641a6ad522f
TimeSeriesLord <fearless.smarty@gmail.com>
2026-08-04 13:36:30 -0400

    Refactor fetch.js to improve command processing

--- fetch.js
+++ fetch.js
@@ -20,7 +20,7 @@ function random_item(items) {
 
 // 28-APr-2021 - taken over "yp" and "mj"
 
-var items = ["d", "mg", "q", "pre", "bv"];
+var items = ["d", "mg", "q", "bv"];
 
 // var DefaultCommand = 'd';
 var DefaultCommand = random_item(items);
